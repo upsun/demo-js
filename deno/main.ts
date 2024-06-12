dc8f50c04aee7d86d221511e9651125c33f80ea8
chadcarlson <chad.carlson@platform.sh>
2024-06-12 16:09:10 -0400

    Update deno.

--- deno/main.ts
+++ deno/main.ts
@@ -1,14 +1,16 @@
 import figlet from "npm:figlet";
-import pshConfig from "npm:platformsh-config";
 
-
-const config = pshConfig.config();
-const port = config.isValidPlatform() ? config.port : 8080;
+const port = Deno.env.get("PLATFORM_APP_DIR") ? Deno.env.get("PORT") : 3003;
 
 const handler = (_req: Request): Response => {
+    const reqPath = new URL(_req.url).pathname;
     const body = figlet.textSync("Deno!");
-    return new Response(body);
-};
+    if (reqPath === "/deno") {
+        return new Response(body);
+    } else {
+      return Response.redirect(_req.url + "deno", 302);;
+    }
 
+};
 
 Deno.serve({ port }, handler);
