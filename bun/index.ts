21c421d9136e8c0f20537d510bd9d0139e5049a6
chadcarlson <chad.carlson@platform.sh>
2024-06-12 14:03:13 -0400

    update subpath.

--- bun/index.ts
+++ bun/index.ts
@@ -5,7 +5,8 @@ const config = require("platformsh-config").config();
 Bun.serve({
   port: config.isValidPlatform() ? config.port : 3000,
   fetch(_req: Request): Response {
+    const url = new URL(_req.url);
     const body = figlet.textSync("Bun!");
-    return new Response(body);
+    if (url.pathname === "/bun") return new Response(body);
   },
 });
