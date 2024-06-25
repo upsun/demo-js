2fccdffcd70f90f3f8e89746817dbea6dff1a86c
chadcarlson <chad.carlson@platform.sh>
2024-06-25 14:46:16 -0400

    Fix turborepo local, check remote.

---
+++ shell.nix
@@ -0,0 +1,21 @@
+let
+  nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-24.05";
+  pkgs = import nixpkgs { config = {}; overlays = []; };
+in
+
+pkgs.mkShellNoCC {
+  packages = with pkgs; [
+    bun
+    nodejs_22
+    deno
+    nodePackages.npm
+  ];
+  DENO_DIR="./cache";
+  shellHook = ''
+    ROOT=$(pwd)
+    npm install --workspace=main --workspace=nodejs
+    cd $ROOT/apps/bun && bun install
+    cd $ROOT/apps/deno && deno cache package.json
+    cd $ROOT && npm run build
+  '';
+}
