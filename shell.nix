ee63112c6e495eb4812f634af5b1718a3e6b39b2
chadcarlson <chad.carlson@platform.sh>
2024-06-26 11:05:32 -0400

    Add compile step.

--- shell.nix
+++ shell.nix
@@ -15,7 +15,7 @@ pkgs.mkShellNoCC {
     ROOT=$(pwd)
     npm install --workspace=main --workspace=nodejs
     cd $ROOT/apps/bun && bun install
-    cd $ROOT/apps/deno && deno cache package.json
+    cd $ROOT/apps/deno && deno cache package.json && deno compile --allow-env --allow-net --allow-read main.ts
     cd $ROOT && npm run build
   '';
 }
